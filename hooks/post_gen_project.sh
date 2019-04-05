4a2790f76d241449f14d9813a9f6a7b6cade2c7d
Gert-Jan <gert-jan.both@cri-paris.com>
2019-04-05 17:10:26 +0200

    Fixed automatic template addition

--- hooks/post_gen_project.sh
+++
@@ -1,7 +0,0 @@
-cd {{cookiecutter.project_name}}/config
-
-template_path=$PWD
-cd $HOME/.jupyter
-
-echo "c.JupyterLabTemplates.template_dirs = ["$template_path"]" >> jupyter_notebook_config.py
-echo "c.JupyterLabTemplates.include_default = False" >> jupyter_notebook_config.py
