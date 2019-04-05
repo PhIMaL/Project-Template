8a70ff3001d59b63d745fcc177870d24ee8f0bbd
Gert-Jan <gert-jan.both@cri-paris.com>
2019-04-05 17:50:38 +0200

    Added environment file.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,14 +7,3 @@ template_path = os.path.join(os.getcwd(), 'config/templates/')
 with open(config_path, 'a') as file:
     file.write("c.JupyterLabTemplates.include_default = False\n")
     file.write('c.JupyterLabTemplates.template_dirs = ["' + str(template_path) + '"]\n')
-
-#cd ${{cookiecutter.project_name}}
-#cd config/
-
-#echo "$PWD"
-#template_path=$PWD
-#cd $HOME/.jupyter
-
-#echo "c.JupyterLabTemplates.template_dirs = ["$template_path"]" >> jupyter_notebook_config.py
-#exit 0
-#echo "c.JupyterLabTemplates.include_default = False" >> jupyter_notebook_config.py
